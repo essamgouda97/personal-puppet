0a74a03b6ef96a03dc1334b9586c2a835f85edf4
Essam Gouda <essamgouda97@hotmail.com>
2022-04-27 23:53:00 -0600

    added openssh-server in utilities.pp

--- modules/packages/manifests/utilities.pp
+++ modules/packages/manifests/utilities.pp
@@ -2,7 +2,7 @@ class packages::utilities {
   $tools = [
     'curl', 'dos2unix', 'ffmpeg', 'graphviz', 'inotify-tools', 'jq', 'mlocate',
     'ncdu', 'neofetch', 'net-tools', 'rlwrap', 'sqlite3', 'tmux', 'tree',
-    'xclip',
+    'xclip', 'openssh-server',
   ]
   package { $tools: ensure => 'latest' }
 }
